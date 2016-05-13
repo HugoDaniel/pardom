c45899190eaf1ba8156c1ebc97d054b8c380ac18
Hugo Daniel <mr.hugo.gomes@gmail.com>
2016-05-13 22:56:38 +0100

    Hello world example

--- examples/hello_world/hello.js
+++ examples/hello_world/hello.js
@@ -1,25 +1,17 @@
-console.log("[worker]", "IN WORKER");
+importScripts('hello_constants.js');
 
 onmessage = function(e) {
-	console.log("[worker]", "got message", e.data);
-	// this.postMessage({ type: "MEH", action: "YESSIR", args: [1,2] });
+	switch(e.data) {
+		case 'PARDOM':
+			this.postMessage({ type: DOM_READ_TYPE, action: READ_INITIAL_STUFF });
+		break;
+		case INIT_DONE:
+			this.postMessage({ type: REGISTER_EVENT_TYPE, action: ON_CLICK });
+		break;
+		case ON_CLICK:
+			this.postMessage({ type: DOM_WRITE_TYPE, action: HELLO });
+		break;
+		default:
+			console.log('[worker]', 'unhandled message', e.data);
+	}
 }
-
-setTimeout( function() {
-	this.postMessage({ type: "BAM", action: "YESSIR", args: [4,5] });
-	this.postMessage({ type: "BAM", action: "YESSIR", args: [3,3] });
-	this.postMessage({ type: "BAM", action: "YESSIR", args: [7,7] });
-	this.postMessage({ type: "BAM", action: "YESSIR", args: [10,20] });
-	this.postMessage({ type: "BAM", action: "YESSIR", args: [7] });
-	this.postMessage({ type: "BAM", action: "YESSIR", args: [1024] });
-	this.postMessage({ type: "BAM", action: "YESSIR", args: [2048] });
-	this.postMessage({ type: "BAM", action: "YESSIR", args: [4096] });
-	this.postMessage({ type: "MEH", action: "YESSIR", args: [10,20] });
-	this.postMessage({ type: "MEH", action: "YESSIR", args: [2000,20000] });
-	this.postMessage({ type: "MEH", action: "YESSIR", args: [10,20] });
-	this.postMessage({ type: "MEH", action: "YESSIR", args: [2000,20000] });
-	this.postMessage({ type: "FOO", action: "BAR", args: [Math.random(), Math.random()] });
-	this.postMessage({ type: "FOO", action: "BAR", args: [Math.random(), Math.random()] });
-	this.postMessage({ type: "FOO", action: "BAR", args: [Math.random(), Math.random()] });
-	this.postMessage({ type: "FOO", action: "BAR", args: [Math.random(), Math.random()] });
-}.bind(this), 4000);
